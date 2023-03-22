d212c5a2a7ca039eebd6bd49fef891c5a66766a8
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-22 00:34:58 -0600

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
+import tempfile
+import uuid
+import os
+
+
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding='utf-8') as new_file:
+        new_file.write(file_content)
+        
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding='utf-8') as new_file:
+        new_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    text_content = open(f"{file_name}.txt")
+    return text_content.read()
+
+write_file("./fuck", "bitch ass")
+read_file("./fuck")
